cbc2b0f69eeb20df37de15b5b439b3e7e2181c35
Mythili Pappu <mythilipappu@Mythilis-MacBook-Air.local>
2023-04-06 13:44:38 -0500

    Initial commit

---
+++ src/test/java/com/MavenPractice/MavenPractice/MavenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MavenPractice.MavenPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
